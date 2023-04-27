a98568124f6911b1bd6e33254a8500f6edba9b1f
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-04-28 00:34:21 +0500

    feat: canban app models

--- apps/canban/models.py
+++ apps/canban/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.db.models import CharField, TextField, ForeignKey, CASCADE
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class Board(BaseModel):
+    name = CharField(max_length=256)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'board'
+        verbose_name = "Board"
+        verbose_name_plural = "Boards"
+
+
+class Status(BaseModel):
+    board = ForeignKey('canban.Board', CASCADE, 'status')
+    name = CharField(max_length=256)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'ststus'
+        verbose_name = 'Status'
+        verbose_name_plural = 'Status'
+
+
+class Task(BaseModel):
+    status = ForeignKey('canban.Status', CASCADE, 'tasks')
+    title = CharField(max_length=256)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'task'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+
+class Subtask(BaseModel):
+    task = ForeignKey('canban.Task', CASCADE, 'subtasks')
+    title = CharField(max_length=256)
+    description = TextField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'subtask'
+        verbose_name = "Subtask"
+        verbose_name_plural = 'Subtasks'
