e05586757ce7c76aad5519ab0bd2c4675b4fa379
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-05-04 12:38:25 +0500

    fix:change swagger url to base

--- core/schema.py
+++ core/schema.py
@@ -33,6 +33,6 @@ schema_view = get_schema_view(
 # ]
 swagger_urlpatterns = [
     re_path(r"^doc(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("doc/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
