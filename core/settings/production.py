f890c60e47039336a5a85a129ecda31e8ca9f7ab
Muhammad <92747161+muhammadjabborov@users.noreply.github.com>
2023-05-06 01:09:53 +0500

    Update production.py

---
+++ core/settings/production.py
@@ -0,0 +1,28 @@
+from .base import *  # noqa
+
+###################################################################
+# General
+###################################################################
+
+DEBUG = False
+
+###################################################################
+# Django security
+###################################################################
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://104.248.251.121"
+]
+
+###################################################################
+# CORS
+###################################################################
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ["*"]
+
