91c71f97a0e75e0b58b67a423e7c21a3726a9c90
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-07-27 11:20:38 +0500

    r.m c.t

--- core/schema.py
+++ core/schema.py
@@ -32,7 +32,6 @@ schema_view = get_schema_view(
 #     ),
 # ]
 swagger_urlpatterns = [
-    re_path(r"^doc(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
